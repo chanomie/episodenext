0d2f160136f2ec9117abea1e94333f4ef7e97f87
Jordan Reed <jreed@extole.com>
2013-11-21 22:40:21 -0800

    Updated to support the resizing the images when adding to MemCache

--- src/main/java/net/chaosserver/wiredepisodes/WatchedEpisodeCache.java
+++ src/main/java/net/chaosserver/wiredepisodes/WatchedEpisodeCache.java
@@ -2,6 +2,8 @@ package net.chaosserver.wiredepisodes;
 
 import java.util.Collections;
 import java.util.Set;
+import java.util.logging.Logger;
+import java.util.logging.Level;
 
 import javax.cache.Cache;
 import javax.cache.CacheException;
@@ -9,15 +11,45 @@ import javax.cache.CacheFactory;
 import javax.cache.CacheManager;
 
 import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.images.Image;
+import com.google.appengine.api.images.ImagesService;
+import com.google.appengine.api.images.ImagesServiceFactory;
+import com.google.appengine.api.images.OutputSettings;
+import com.google.appengine.api.images.Transform;
+import com.google.appengine.api.memcache.MemcacheServiceException;
 
+/**
+ * Manages all of the caches associated with the system.  This is using
+ * the Java Cache API, but that uses Google MemCache under the covers inside
+ * of app engine.
+ * 
+ * @author jreed
+ *
+ */
 public class WatchedEpisodeCache {
+	/** Logger. */
+	private static final Logger log = Logger.getLogger(WatchedEpisodeCache.class
+			.getName());
+
+	/** Reference to the Image Service used to resize overly large images. */
+	ImagesService imagesService;
+	
+	/** Cache of all the watched keys. */
 	Cache watchedKeysCache;
+	
+	/** Cache of all the series banner images. */
 	Cache seriesImageCache;
 	
+	/**
+	 * Constructor initializes all of the internal objects.
+	 * 
+	 * @throws CacheException
+	 */
 	public WatchedEpisodeCache() throws CacheException {
 		CacheFactory cacheFactory = CacheManager.getInstance().getCacheFactory();
 		watchedKeysCache = cacheFactory.createCache(Collections.emptyMap());
 		seriesImageCache = cacheFactory.createCache(Collections.emptyMap());
+		imagesService = ImagesServiceFactory.getImagesService();
 	}
 
 	public Set<String> getWatchedEpisodesKeys(Key principalKey) {
@@ -35,16 +67,25 @@ public class WatchedEpisodeCache {
 	}
 	
 	public void putEpisodeImage(String path, byte[] image) {
-		// If it must be recached
-		// compress & resize it
-		/*
-		OutputSettings settings = new OutputSettings(ImagesService.OutputEncoding.JPEG);
-		settings.setQuality(90);
-		Transform transform = ImagesServiceFactory.makeResize(newWidth, newHeight) 
-		Image newImage = imagesService.applyTransform(transform, oldImage, settings);
-		byte[] blobData = newImage.getImageData();
-		*/
+		int quality = 90;
+		int imageLength = image.length;
+		byte[] imageToCache = image;
 		
-		seriesImageCache.put(path, image);
+		while(imageLength >= 1000000 && quality > 0) {
+			OutputSettings settings = new OutputSettings(ImagesService.OutputEncoding.JPEG);
+			Image oldImage = ImagesServiceFactory.makeImage(image);
+			Transform transform = ImagesServiceFactory.makeResize(oldImage.getWidth(), oldImage.getHeight());
+			settings.setQuality(quality);
+			Image newImage = imagesService.applyTransform(transform, oldImage, settings);
+			imageToCache = newImage.getImageData();
+			imageLength = imageToCache.length;
+			quality =- 10;
+		}
+		
+		try {
+			seriesImageCache.put(path, imageToCache);
+		} catch (MemcacheServiceException e) {
+			log.log(Level.WARNING, "Failed to put into cache", (Throwable) e);
+		}		
 	}
 }
